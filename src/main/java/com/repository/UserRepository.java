97f7e2c1ca0c30d46e51c03a6bc5efaa9f1b052c
Muhammad Adeeb Ansari <96252525+adeeb4098@users.noreply.github.com>
2023-11-05 12:20:13 +0530

    new things added

--- src/main/java/com/repository/UserRepository.java
+++ src/main/java/com/repository/UserRepository.java
@@ -2,9 +2,8 @@ package com.repository;
 
 import com.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
+
 public interface UserRepository extends JpaRepository<User, Long> {
     public User findByEmail(String email);
 }
