3c667750b7937d504d5da7ef030eb77fc619d90f
Muhammad Adeeb Ansari <96252525+adeeb4098@users.noreply.github.com>
2023-11-05 11:07:35 +0530

    new things added

--- src/main/java/com/ecommerce_backend/EcommerceBackendApplication.java
+++ src/main/java/com/ecommerce_backend/EcommerceBackendApplication.java
@@ -3,13 +3,14 @@ package com.ecommerce_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EntityScan(basePackages = "com.model")
 public class EcommerceBackendApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 	}
 }
