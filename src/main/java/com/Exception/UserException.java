ede2292b5afb4c54028c47f450533406c6e37b7d
Muhammad Adeeb Ansari <96252525+adeeb4098@users.noreply.github.com>
2024-02-11 13:19:16 +0530

    new_things

--- src/main/java/com/Exception/UserException.java
+++
@@ -1,7 +0,0 @@
-package com.Exception;
-
-public class UserException extends Exception {
-    public UserException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.config;
-
-public class JwtConstant {
-
-    public static final String SECRET_KEY="bahjgtwebbvdsmgysduekuehisk.hfdgksyebfykgbse,zrueu";
-    public static final String JWT_HEADER="Authorization";
-}
